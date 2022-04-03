10d47a89ebf1908823bc2c7e880f6733eb7527b1
Jan Zelinka <jan.zelinka12@gmail.com>
2022-04-03 17:58:39 +0200

    added products controller

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -10,56 +10,54 @@ import {
   request,
   response,
 } from 'inversify-express-utils'
-import { Customer } from '../database/entity/Customer'
-import { AUTHORIZED_MIDDLEWARE } from '../middlewares'
+import { Product } from '../database/entity/Product'
 import { AbstractRepository } from '../repository/Repository'
 import { DatabaseService } from '../services/DatabaseService'
-import { ONLY_USERS } from './permissions/Permissions'
 
-@controller('/customers')
-export class CustomersController
-  extends AbstractRepository<Customer>
+@controller('/products')
+export class ProductsController
+  extends AbstractRepository<Product>
   implements interfaces.Controller
 {
   constructor(
     @inject(DatabaseService) private databaseService: DatabaseService
   ) {
-    super(databaseService.getRepository(Customer))
+    super(databaseService.getRepository(Product))
   }
 
-  @httpGet('/', AUTHORIZED_MIDDLEWARE(ONLY_USERS))
+  @httpGet('/')
   private async _get(
     @request() req: express.Request,
     @response() res: express.Response
   ) {
-    console.log('hit the get')
+    // console.log('hit the get')
     return this.get()
   }
 
-  @httpPost('/create', AUTHORIZED_MIDDLEWARE(ONLY_USERS))
+  @httpPost('/create')
   private async _create(
     @request() req: express.Request,
     @response() res: express.Response
   ) {
-    console.log('hit the create')
-    return this.create(req.body)
+    // console.log('hit the create')
+    // return this.create(req.body)
   }
 
-  @httpPut('/update', AUTHORIZED_MIDDLEWARE(ONLY_USERS))
+  @httpPut('/update')
   private async _update(
     @request() req: express.Request,
     @response() res: express.Response
   ) {
-    console.log('hit the update', req.body)
-    return this.update(req.body.id, req.body)
+    // console.log('hit the update', req.body)
+    // return this.update(req.body.id, req.body)
   }
 
-  @httpDelete('/delete', AUTHORIZED_MIDDLEWARE(ONLY_USERS))
+  @httpDelete('/delete')
   private async _delete(
     @request() req: express.Request,
     @response() res: express.Response
   ) {
-    console.log('hit the delete')
-    return this.delete(req.body.id)
+    // console.log('hit the delete')
+    // return this.delete(req.body.id)
   }
 }
